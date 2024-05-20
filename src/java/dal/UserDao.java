6a6bf7022fea7f9f0748884ed5628c4de63203f3
Thinhnthe172159 <thinhnthe172159@fpt.edu.vn>
2024-05-20 18:10:25 +0700

    try this

---
+++ src/java/dal/UserDao.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dal;
+
+import dal.DBContext;
+import model.User;
+
+/**
+ *
+ * @author thinh
+ */
+public class UserDao extends DBContext{
+    
+    public void inputUser(User u){
+        String sql ="";
+    }
+}
