293e088cfad5e2d0a5b99b60b8ec4c006d2d5bd3
MOONKNIGHT\ASUS <Huyldhe172154@fpt.edu.vn>
2024-06-18 05:06:41 +0700

    Update Dashboard

--- src/java/model/User.java
+++ src/java/model/User.java
@@ -11,7 +11,7 @@ import java.sql.Date;
  * @author thinh
  */
 public class User {
-    
+
     private int id;
     private String email;
     private String password;
@@ -25,7 +25,7 @@ public class User {
 
     public User() {
     }
-    
+
     public User(String email, String password, Role role_id, int status, String first_name, String last_name, Date dob, String image, double money) {
         this.email = email;
         this.password = password;
@@ -51,15 +51,17 @@ public class User {
         this.money = money;
     }
 
-    public User(int id, String email, String first_name, String last_name, Date dob, double money) {
+    public User(int id, String email, Role role_id, int status, String first_name, String last_name, Date dob, String image, double money) {
         this.id = id;
         this.email = email;
+        this.role_id = role_id;
+        this.status = status;
         this.first_name = first_name;
         this.last_name = last_name;
         this.dob = dob;
+        this.image = image;
         this.money = money;
     }
-    
 
     public int getId() {
         return id;
@@ -140,13 +142,10 @@ public class User {
     public void setMoney(double money) {
         this.money = money;
     }
-    
 
     @Override
     public String toString() {
         return "User{" + "id=" + id + ", email=" + email + ", password=" + password + ", role_id=" + role_id + ", status=" + status + ", first_name=" + first_name + ", last_name=" + last_name + ", dob=" + dob + ", image=" + image + '}';
     }
-    
-    
-    
+
 }
