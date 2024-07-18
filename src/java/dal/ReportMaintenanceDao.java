33412218bb955445ba9faf26eee5ee8994c1695c
MOONKNIGHT\ASUS <Huyldhe172154@fpt.edu.vn>
2024-07-18 23:47:52 +0700

    update report maintenance

--- src/java/dal/ReportMaintenanceDao.java
+++ src/java/dal/ReportMaintenanceDao.java
@@ -106,6 +106,57 @@ public class ReportMaintenanceDao extends DBContext {
         return reports;
     }
 
-    
+    public ReportMaintain getReportById(int reportId) {
+        ReportMaintain report = null;
+        String strSQL = "SELECT rm.*, a.name as apartment_name, t.first_name as tenant_first_name, t.last_name as tenant_last_name, t.image as tenant_image "
+                + "FROM Report_maintain rm "
+                + "JOIN Aparment a ON rm.apartment_id = a.id "
+                + "JOIN [User] t ON rm.tenant_id = t.id "
+                + "WHERE rm.id = ?";
+        try {
+            pstm = cnn.prepareStatement(strSQL);
+            pstm.setInt(1, reportId);
+            ResultSet rs = pstm.executeQuery();
+            if (rs.next()) {
+                Apartment apartment = new Apartment();
+                apartment.setId(rs.getInt("apartment_id"));
+                apartment.setName(rs.getString("apartment_name"));
+
+                User tenant = new User();
+                tenant.setId(rs.getInt("tenant_id"));
+                tenant.setFirst_name(rs.getString("tenant_first_name"));
+                tenant.setLast_name(rs.getString("tenant_last_name"));
+                tenant.setImage(rs.getString("tenant_image"));
+
+                report = new ReportMaintain();
+                report.setId(rs.getInt("id"));
+                report.setApartmentId(apartment);
+                report.setTenantId(tenant);
+                report.setContext(rs.getString("context"));
+                report.setStatus(rs.getString("status"));
+                report.setDatePost(rs.getDate("date_post"));
+                report.setImage1(rs.getString("image1"));
+                report.setImage2(rs.getString("image2"));
+                report.setImage3(rs.getString("image3"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return report;
+    }
+
+    public boolean updateReportStatus(int reportId, int newStatus) {
+        String strSQL = "UPDATE Report_maintain SET status = ? WHERE id = ?";
+        try {
+            pstm = cnn.prepareStatement(strSQL);
+            pstm.setInt(1, newStatus);
+            pstm.setInt(2, reportId);
+            pstm.executeUpdate();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
 
 }
